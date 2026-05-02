f0124d9eda6337c159cb5fa41dff3077dd333609
jangrarajat <rajatjangra940@gmail.com>
2026-05-02 20:20:57 +0530

    trying to solving deploying ishu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This helps with relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
